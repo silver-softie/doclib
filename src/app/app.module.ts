2b3ff63529472fa96dfac7f9559f1c95b255bd79
Brad Moffat <brad@moffat.me.uk>
2021-02-14 08:58:38 +0000

    Moved search into document list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { SearchComponent } from './header/search/search.component';
+import { SearchComponent } from './doc-list/search/search.component';
 import { DocListComponent } from './doc-list/doc-list.component';
 import { DocItemComponent } from './doc-list/doc-item/doc-item.component';
 import { UserManagementComponent } from './user-management/user-management.component';
